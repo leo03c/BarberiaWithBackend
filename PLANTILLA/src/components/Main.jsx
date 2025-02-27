94fda90062b77a0f49e39edcd0c0e8d318a6f89a
Andywrld <andy18wrld@gmail.com>
2025-02-27 08:06:59 -0500

    update main

--- PLANTILLA/src/components/Main.jsx
+++ PLANTILLA/src/components/Main.jsx
@@ -1,21 +1,21 @@
 // import React from 'react'
-import Hero from "./Hero";
-import Services from "./Services";
-import FeaturedProducts from "./FeaturedProducts";
-import AboutSection from "./AboutSection";
-import Testimonials from "./Testimonials";
-import CTA from "./CTA";
+import Hero from './Hero';
+import Services from './Services';
+import AboutSection from './AboutSection';
+import Testimonials from './Testimonials';
+import CTA from './CTA';
+import Galeria from './Galeria';
 function Main() {
   return (
     <div>
       <Hero></Hero>
       <Services></Services>
-      <FeaturedProducts></FeaturedProducts>
+      <Galeria />
       <AboutSection></AboutSection>
       <Testimonials></Testimonials>
       <CTA></CTA>
     </div>
-  )
+  );
 }
 
-export default Main
+export default Main;
