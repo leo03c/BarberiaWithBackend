64fa89406022f2891a887c256ee2fe382e4a9c6d
Andywrld <andy18wrld@gmail.com>
2025-02-27 08:06:11 -0500

    routes

--- PLANTILLA/src/App.jsx
+++ PLANTILLA/src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import Header from "./components/Header";
-import Main from "./components/main";
-import Footer from "./components/Footer";
-import Compra from "./components/Compra";
-import ReservationPage from "./components/ReservationPage";
-import ClienteForm from "./components/ClienteForm";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Header from './components/Header';
+import Main from './components/main';
+import Footer from './components/Footer';
+import Gallery from './components/Gallery';
+import ReservationPage from './components/ReservationPage';
+import ClienteForm from './components/ClienteForm';
 
 function App() {
   const [user, setUser] = useState(null); // Estado para manejar el usuario autenticado
@@ -13,14 +13,15 @@ function App() {
   return (
     <Router>
       <Header user={user} setUser={setUser} /> {/* Pasamos user y setUser */}
-      
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/reserva" element={<ReservationPage />} />
-        <Route path="/clienteform" element={<ClienteForm setUser={setUser} />} />
-        <Route path="/compra" element={<Compra />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/reserva' element={<ReservationPage />} />
+        <Route
+          path='/clienteform'
+          element={<ClienteForm setUser={setUser} />}
+        />
+        <Route path='/galeria/' element={<Gallery />} />
       </Routes>
-
       <Footer />
     </Router>
   );
