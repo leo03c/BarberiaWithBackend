ebecbd980f8578cae0e69ba35ebd84bbf0046c0b
Andywrld <andy18wrld@gmail.com>
2025-02-28 14:42:21 -0500

    creando el simplejwt

--- BackendDj/BackendDj/settings.py
+++ BackendDj/BackendDj/settings.py
@@ -64,6 +64,7 @@ CORS_ALLOWED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
 
