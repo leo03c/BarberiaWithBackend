c26aa09758540ecc511fc72a8b12b640ebae300e
Andywrld <andy18wrld@gmail.com>
2025-02-27 22:05:26 -0500

    custom hook fecth

---
+++ PLANTILLA/src/hook/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        if (!response.ok) throw new Error('Error al obtener los datos');
+        setData(jsonData);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+};
