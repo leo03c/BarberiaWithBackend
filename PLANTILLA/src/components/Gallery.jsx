9249782bffad585d2ccd27e1d80737b0d72871cc
Andywrld <andy18wrld@gmail.com>
2025-02-27 22:05:42 -0500

    update gallery

--- PLANTILLA/src/components/Gallery.jsx
+++ PLANTILLA/src/components/Gallery.jsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useFetch } from '../hook/useFetch';
 
 const Gallery = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   const url = 'http://127.0.0.1:8000/fotos/';
 
-  const fetchData = async () => {
-    setLoading(true);
-
-    try {
-      const response = await fetch(url);
-      const jsonData = await response.json();
-
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos');
-      }
-
-      setData(jsonData);
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data, isLoading, error } = useFetch(url);
 
   if (isLoading) return <div>Cargando...</div>;
 
