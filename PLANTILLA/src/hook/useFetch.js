3a193afb718f1ce7e65c2c1ab00e10c798eff1f0
Andywrld <andy18wrld@gmail.com>
2025-02-28 10:24:02 -0500

    use fetch update

--- PLANTILLA/src/hook/useFetch.js
+++ PLANTILLA/src/hook/useFetch.js
@@ -6,12 +6,10 @@ export const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    let controller = new AbortController();
-
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await fetch(url, controller);
+        const response = await fetch(url);
         const jsonData = await response.json();
         if (!response.ok) throw new Error('Error al obtener los datos');
         setData(jsonData);
@@ -22,10 +20,6 @@ export const useFetch = (url) => {
       }
     };
     fetchData();
-
-    return () => {
-      controller.abort();
-    };
   }, [url]);
 
   return { data, isLoading, error };
