28c23ef1183c1c157c923e6243e0742fe927b2ab
Andywrld <andy18wrld@gmail.com>
2025-02-28 19:50:08 -0500

    CustomTOKEN

--- BackendDj/api/views.py
+++ BackendDj/api/views.py
@@ -28,6 +28,21 @@ from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import CustomTokenObtainPairSerializer
+
+class CustomTokenObtainPairView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        print("Datos recibidos en Django:", request.data)
+        serializer = CustomTokenObtainPairSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 # Create your views here.
 class ProductoFilter(filters.FilterSet):
     class Meta:
