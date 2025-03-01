f4d6300224409171c7453aa8b72c32be62923abf
Andywrld <andy18wrld@gmail.com>
2025-02-28 19:49:38 -0500

    creando la ruta del login

--- BackendDj/BackendDj/urls.py
+++ BackendDj/BackendDj/urls.py
@@ -19,14 +19,10 @@ from django.urls import path
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('api.urls') ),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('api.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
