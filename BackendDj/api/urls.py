458bf189728de8a2ab375a867a7e7076940a23d4
Andywrld <andy18wrld@gmail.com>
2025-03-10 16:38:24 -0400

    update api urls

--- BackendDj/api/urls.py
+++ BackendDj/api/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 from . import views
-from .views import CustomTokenObtainPairView, RegistroClienteView
+from .views import CustomTokenObtainPairView, RegistroUsuarioView,AdminDashboardView
 
 router = routers.DefaultRouter()
 router.register(r'productos', views.ProductoViewSet, basename='productos')
-router.register(r'clientes', views.ClienteViewSet, basename='clientes')
+router.register(r'usuarios', views.UsuarioViewSet, basename='usuarios')
 router.register(r'promociones', views.PromocionViewSet, basename='promociones')
 router.register(r'reseñas', views.ReseñaViewSet, basename='reseñas')
 router.register(r'pagos', views.PagoViewSet, basename='pagos')
@@ -17,5 +17,6 @@ router.register(r'citas', views.CitaViewSet, basename='citas')
 urlpatterns = [
     path('', include(router.urls)),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('registro/', RegistroClienteView.as_view(), name='registro_cliente'),
+    path('registro/', RegistroUsuarioView.as_view(), name='registro_cliente'),
+     path('api/dashboard/', AdminDashboardView.as_view(), name='admin_dashboard'),
 ]
