f2ebd533960646940dccb966a58cd664418eca84
Andywrld <andy18wrld@gmail.com>
2025-03-10 16:37:36 -0400

    panel admin

--- BackendDj/api/admin.py
+++ BackendDj/api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Producto
 from .models import Cita
-from .models import Cliente
+from .models import Usuario
 from .models import Foto
 from .models import Promocion
 from .models import Reseña
@@ -13,7 +13,7 @@ from .models import Pago
 
 admin.site.register(Producto)
 admin.site.register(Cita)
-admin.site.register(Cliente)
+admin.site.register(Usuario)
 admin.site.register(Foto)
 admin.site.register(Promocion)
 admin.site.register(Reseña)
