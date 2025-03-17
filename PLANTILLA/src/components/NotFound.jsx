4b64440fb254bc28ea0c3dd881962f1d953a69d6
Leo <leonardocastillod7@gmail.com>
2025-03-17 19:45:47 -0400

    boberias

--- PLANTILLA/src/components/NotFound.jsx
+++ PLANTILLA/src/components/NotFound.jsx
@@ -2,14 +2,14 @@ import Ruanu from '../assets/hkRuanu.png';
 
 export const NotFound = () => {
   return (
-    <div className='  flex justify-center items-center min-h-screen bg-gray-500 '>
+    <div className='  flex justify-center items-center min-h-screen bg-jetBlack '>
       <div className='flex flex-col items-center w-1/2 '>
         <div className='w-96'>
           <img src={Ruanu} alt='' className='w-full' />
         </div>
 
-        <div className='w-full'>
-          <h2 className='font-semibold text-4xl font-serif text-center'>
+        <div className='w-full text-lightGray'>
+          <h2 className='font-semibold text-4xl font-serif text-center '>
             This Page is not Real
           </h2>
           <p className='mt-10'>
