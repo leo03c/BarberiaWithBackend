cd5c50e0ea2c917ff9d97f80873fdf72c0b9901b
Leo <leonardocastillod7@gmail.com>
2025-03-17 18:41:40 -0400

    cambio en los iconos

--- PLANTILLA/src/components/dashboard/Sidebar.jsx
+++ PLANTILLA/src/components/dashboard/Sidebar.jsx
@@ -1,13 +1,11 @@
 import { Link } from "react-router-dom";
-import {  Users, ShoppingBag, Calendar, Percent } from "lucide-react";
+import { Users, ShoppingBag, Calendar, Percent, Image, Star } from "lucide-react";
 
 const Sidebar = () => {
     return (
         <div className="w-64 bg-jetBlack h-full flex flex-col p-6 shadow-lg mt-20">
-
             <nav className="flex-1">
                 <ul className="space-y-3">
-                    
                     <li>
                         <Link to="/dashboard/tusuario" className="flex items-center gap-2 p-3 rounded-md hover:bg-mustard/20 transition">
                             <Users size={20} className="text-mustard" /> Usuarios
@@ -35,7 +33,7 @@ const Sidebar = () => {
                     </li>
                     <li>
                         <Link to="/dashboard/tfotos" className="flex items-center gap-2 p-3 rounded-md hover:bg-mustard/20 transition">
-                            <Calendar size={20} className="text-mustard" /> Fotos
+                            <Image size={20} className="text-mustard" /> Fotos
                         </Link>
                     </li>
                     <li>
@@ -45,7 +43,7 @@ const Sidebar = () => {
                     </li>
                     <li>
                         <Link to="/dashboard/treseñas" className="flex items-center gap-2 p-3 rounded-md hover:bg-mustard/20 transition">
-                            <Percent size={20} className="text-mustard" /> Reseñas
+                            <Star size={20} className="text-mustard" /> Reseñas
                         </Link>
                     </li>
                 </ul>
