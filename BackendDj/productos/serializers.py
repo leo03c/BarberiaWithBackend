c5663f1153ed42ec63c19afee004862aa9bce40d
Andy Torres <andy18wrld@gmail.com>
2025-05-30 22:43:17 -0400

    Cambios en el serializer producto

--- BackendDj/productos/serializers.py
+++ BackendDj/productos/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 from .models import Producto
+from rest_framework.validators import UniqueValidator
+
 
 
 
 
 class ProductoSerializer(serializers.ModelSerializer):
+ 
     class Meta:
         model = Producto
         fields = '__all__'
@@ -15,7 +18,11 @@ class ProductoSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("El valor debe ser mayor que cero.")
         return value
     
+    
+    
     def validate_cantidad(self, value):
         if value < 0:
             raise serializers.ValidationError("La cantidad no puede ser negativa.")
         return value
+    
+  
