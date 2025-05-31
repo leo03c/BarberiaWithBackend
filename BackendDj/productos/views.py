0a4a3e637550fde5d3c50f38256cc93d9a8eb303
Andy Torres <andy18wrld@gmail.com>
2025-05-30 22:43:24 -0400

    Cambio en el view producto

--- BackendDj/productos/views.py
+++ BackendDj/productos/views.py
@@ -2,6 +2,9 @@ from django_filters import rest_framework as filters
 from rest_framework import viewsets
 from .models import Producto
 from .serializers import ProductoSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 
 # Create your views here.
@@ -16,4 +19,20 @@ class ProductoViewSet(viewsets.ModelViewSet):
     serializer_class = ProductoSerializer
     filter_backends = [filters.DjangoFilterBackend]  # Requiere django-filter
     filterset_class = ProductoFilter
+    
+    
+class ProductoView(APIView):
+    permission_classes = [AllowAny]
+        
+    def post (self, request):
+        serializer = ProductoSerializer(data=request.data)
+
+        if(Producto.objects.filter(nombre=request.data.get('nombre')).exists()):
+            return Response({"nombre": "Este producto ya existe"}, status=400)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+    
+    
 
