a90eb0382a81f2e4716964cf585e3f4e5c5bacbf
Andy Torres <andy18wrld@gmail.com>
2025-05-30 22:43:47 -0400

    cambio serializer trabajadores

--- BackendDj/trabajadores/serializers.py
+++ BackendDj/trabajadores/serializers.py
@@ -16,15 +16,14 @@ class TrabajadorSerializer(serializers.ModelSerializer):
       
         fecha_nacimiento = ci[:6]
 
-        # Verificar que los primeros 6 dígitos sean numéricos
+        
         if not fecha_nacimiento.isdigit():
             raise serializers.ValidationError('Ingrese un carnet valido')
 
-        año = int(fecha_nacimiento[:2])  # Los primeros 2 dígitos (Año)
-        mes = int(fecha_nacimiento[2:4])  # Los siguientes 2 dígitos (Mes)
-        dia = int(fecha_nacimiento[4:6])  # Los últimos 2 dígitos (Día)
+        año = int(fecha_nacimiento[:2])  
+        mes = int(fecha_nacimiento[2:4])  
+        dia = int(fecha_nacimiento[4:6])  
 
-        # Comprobar que el mes sea válido (debe estar entre 01 y 12)
         if not (1 <= mes <= 12):
             raise serializers.ValidationError('Ingrese un carnet valido')
 
@@ -36,7 +35,7 @@ class TrabajadorSerializer(serializers.ModelSerializer):
     
     def validate_salario(self,salario):
         if salario < 0:
-            raise serializers.ValidationError("EL salario no puede ser nefativo") 
+            raise serializers.ValidationError("EL salario no puede ser negativo") 
     
         return salario
         
