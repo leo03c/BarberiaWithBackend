1b89c4268c42ecedb9c3c68eecfc3fda005c78df
Andy Torres <andy18wrld@gmail.com>
2025-06-01 17:48:52 -0400

    Query de promociones

---
+++ PLANTILLA/src/hook/reactQuery/usePromociones.js
@@ -0,0 +1,71 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import {
+  createPromociones,
+  updatePromociones,
+  deleteromociones,
+} from '../../api/PromocionesApi';
+export const useCreateTPromociones = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (data) => {
+      createPromociones(data);
+      console.log('data utate',data)
+    },
+    onMutate: async (data) => {
+      qc.cancelQueries(['promociones']);
+
+      const preview = qc.getQueryData(['promociones']);
+
+      qc.setQueryData(['promociones'], (old = []) => [...old, { ...data }]);
+
+      return preview;
+    },
+    onSettled: () => qc.invalidateQueries(['promociones']),
+  });
+};
+
+export const useUpdatePromociones = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, data }) => updatePromociones(id, data),
+    onMutate: async ({ data }) => {
+      await queryClient.cancelQueries({ queryKey: ['promociones'] });
+
+      const previousItems = queryClient.getQueryData(['promociones']);
+
+      queryClient.setQueryData(['promociones'], (old) =>
+        old ? old.map((item) => (item.id === data.id ? data : item)) : []
+      );
+
+      return previousItems;
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['promociones'] });
+    },
+  });
+};
+
+export const useDeletePromociones = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (id) => {
+      return deleteromociones(id);
+    },
+    onMutate: async (id) => {
+      await queryClient.cancelQueries({ queryKey: ['promociones'] });
+
+      const previousItems = queryClient.getQueryData(['promociones']);
+
+      queryClient.setQueryData(['promociones'], (old) =>
+        old ? old.filter((item) => item.id !== id) : []
+      );
+
+      return previousItems;
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['promociones'] });
+    },
+  });
+};
