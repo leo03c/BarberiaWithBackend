74252efe72f0eb27fe865fe21a3b194540d5ff10
Andy Torres <andy18wrld@gmail.com>
2025-06-01 17:48:43 -0400

    Promotios carrusel

--- PLANTILLA/src/components/PromotionsCarousel.jsx
+++ PLANTILLA/src/components/PromotionsCarousel.jsx
@@ -1,52 +1,24 @@
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { GetAllPromociones } from '../api/PromocionesApi';
 
 const PromotionsBar = () => {
-  const [promotions, setPromotions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [services, setServices] = useState([]);
 
-  // Obtener promociones
-  useEffect(() => {
-    fetch('http://localhost:8000/api/promociones/')
-      .then((response) => response.json())
-      .then((data) => setPromotions(data))
-      .catch((error) => console.error('Error fetching promotions:', error));
-  }, []);
-
-  // Obtener los detalles de los servicios de las promociones
-  useEffect(() => {
-    if (promotions.length === 0) return;
-
-    const fetchServices = async () => {
-      try {
-        const servicePromises = promotions.map((promotions) =>
-          fetch(
-            `http://localhost:8000/api/servicios/${promotions.servicio}`
-          ).then((response) => response.json())
-        );
-        const servicesData = await Promise.all(servicePromises);
-        setServices(servicesData);
-      } catch (error) {
-        console.error('Error fetching services:', error);
-      }
-    };
-
-    fetchServices();
-  }, [promotions]);
+  const { data: AllPromotions = [] } = GetAllPromociones();
 
   // Actualizar la promoción cada 7 segundos
   useEffect(() => {
-    if (promotions.length === 0) return;
+    if (AllPromotions.length === 0) return;
 
     const interval = setInterval(() => {
-      setCurrentIndex((prev) => (prev + 1) % promotions.length);
+      setCurrentIndex((prev) => (prev + 1) % AllPromotions.length);
     }, 7000);
 
     return () => clearInterval(interval);
-  }, [promotions.length]);
+  }, [AllPromotions.length]);
 
-  if (promotions.length === 0 || services.length === 0) return null;
+  if (AllPromotions.length === 0) return null;
 
   return (
     <div className='w-full px-8 py-4 bg-jetBlack shadow-md '>
@@ -62,21 +34,21 @@ const PromotionsBar = () => {
           {/* Contenedor de la Promoción */}
           <div className='flex flex-col space-y-2'>
             <span className='text-lg md:text-2xl font-semibold text-mustard uppercase tracking-wide'>
-              {promotions[currentIndex].nombre}
+              {AllPromotions[currentIndex].nombre}
             </span>
             <span className='text-sm md:text-base text-lightGray'>
-              {promotions[currentIndex].descripcion}
+              {AllPromotions[currentIndex].descripcion}
             </span>
           </div>
 
           {/* Información del Servicio y Descuento */}
           <div className='flex flex-col items-end space-y-2'>
             <span className='block text-2xl font-bold text-mustard'>
-              {promotions[currentIndex].porcientoDesc}% OFF
+              {AllPromotions[currentIndex].porcientoDesc}% OFF
             </span>
             <span className='text-sm md:text-base text-lightGray'>
               <span className='font-semibold text-white'>Servicio:</span>{' '}
-              {services[currentIndex]?.nombre || 'Cargando...'}
+              {AllPromotions[currentIndex]?.servicio.nombre || 'Cargando...'}
             </span>
           </div>
         </motion.div>
