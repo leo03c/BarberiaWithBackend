bfd0ee3d47a810af984c3992453cd14d8c94a74c
albesroger <albescaballero@gmail.com>
2025-06-04 04:11:58 -0400

    a

--- BackendDj/api/models.py
+++ BackendDj/api/models.py
@@ -57,7 +57,7 @@ class Foto(models.Model):
 
     nombre = models.CharField(max_length=100)
     imag = models.ImageField(upload_to='galeria/', blank=True, null=True)
-    descripcion = models.CharField(max_length=100)
+    
     
     def __str__(self):
         return self.nombre
