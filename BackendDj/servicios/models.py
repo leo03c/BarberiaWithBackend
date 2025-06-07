dcaa85545b31136516a8f304c1f0b69c7d16a6ab
Andy Torres <andy18wrld@gmail.com>
2025-06-07 01:59:46 -0400

    model unique service

--- BackendDj/servicios/models.py
+++ BackendDj/servicios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import timedelta
 
 class Servicio(models.Model):
-    nombre = models.CharField(max_length=100,blank=False)                                     #unico
+    nombre = models.CharField(max_length=100,blank=False,unique=True)                                     #unico
     precio = models.DecimalField(decimal_places=2,max_digits=7,blank=True)
     descripcion = models.TextField(blank=False)  #opcional
     imagen = models.ImageField(upload_to='servicio/', blank=True, null=True)
